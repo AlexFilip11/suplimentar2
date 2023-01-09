9b55097a0691dbb5086506948f1d9c86f4163e69
Alex Filip <alexfilippaul@gmail.com>
2023-01-10 01:21:59 +0200

    Add project files.

---
+++ suplimentar2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace suplimentar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //• Scrie un program care afiseaza numerele de la 1 la N.
+            //Daca numarul este multiplu de 3, sa se afiseze Fizz, daca este multiplu de 3 sa se afiseze Buzz, daca e multiplu si de 3 si de 5(ex: 15) sa se afiseze FizzBuzz, in restul cazurilor sa se afiseze numarul.
+            Console.WriteLine("Introduceti n ");
+            int n = int.Parse(Console.ReadLine());
+            ShowNumbers(n);
+        }
+        static void ShowNumbers(int n)
+        {
+            for(int i=1; i<=n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else Console.Write(i + " ");
+            }
+        }
+    }
+}
